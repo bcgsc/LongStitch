639b4876806a5e9f5b2745831a865f97d3757cf0
lcoombe <lauren.e.coombe@gmail.com>
2022-03-28 10:04:43 -0700

    Update expected output files for demo

--- tests/run_longstitch_demo.sh
+++ tests/run_longstitch_demo.sh
@@ -15,6 +15,8 @@ set +x
 
 echo ""
 echo "Done tests! Compare your generated files with the files in the expected_outputs folder to ensure the tests were successful."
+echo "Note: Files were generated using Tigmint v1.2.5, ntLink v1.1.3 and ARCS v1.2.3"
+echo ""
 
 echo "Final scaffold files found in: test_scaffolds1.k24.w100.tigmint-ntLink.longstitch-scaffolds.fa test_scaffolds2.k32.w150.tigmint-ntLink-arks.longstitch-scaffolds.fa"
 echo "Tip: compare the fasta files using abyss-fac. Some files might appear slightly different using command line tools like 'cmp' due to sequences being output on different strands."
